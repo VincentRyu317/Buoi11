367b0cb70396a74452e67b56beb4ed8524bafc70
Doan Trong Nhan <doantrongnhan3107@gmail.com>
2023-12-05 20:39:32 +0700

    Bo sung danh sach(nho)

---
+++ QLTN/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Chưa có phần chấm điểm
+//Chưa có danh sách các câu hỏi
+//Nút nộp bài
+
+namespace QLTN
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// 
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Chonquyen());
+        }
+    }
+}
